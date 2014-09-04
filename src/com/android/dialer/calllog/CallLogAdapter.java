mnemonyc/platform_packages_apps_dialer_caf

Add call log search in dialer
- add the support to search call log by name and number in
  dialer for dsds and ssss

Change-Id: I963be0a96e06bf36ba47d042987144b25e8af7d4

src/com/android/dialer/calllog/CallLogAdapter.java
@@ -105,6 +105,8 @@ public class CallLogAdapter extends GroupingListAdapter
     private final CallFetcher mCallFetcher;
     private ViewTreeObserver mViewTreeObserver = null;
 
+    private String mFilterString;
+
     /**
      * A cache of the contact details for the phone numbers in the call log.
      * <p>
@@ -676,7 +678,7 @@ public class CallLogAdapter extends GroupingListAdapter
         // New items also use the highlighted version of the text.
         final boolean isHighlighted = isNew;
         mCallLogViewsHelper.setPhoneCallDetails(views, details, isHighlighted,
-                mUseCallAsPrimaryAction);
+                mUseCallAsPrimaryAction, mFilterString);
 
         if (photoId == 0 && photoUri != null) {
             setPhoto(views, photoUri, lookupUri);
@@ -982,5 +984,9 @@ public class CallLogAdapter extends GroupingListAdapter
             number = matchingNumber;
         }
         return number;
+    }
+
+    public void setQueryString(String filter) {
+        mFilterString = filter;
     }
 }

CarbonTEAM/android_packages_apps_Dialer

Conflicts:
	src/com/android/dialer/calllog/CallLogActivity.java
	src/com/android/dialer/calllog/CallLogAdapter.java
	src/com/android/dialer/calllog/CallLogListItemHelper.java
	tests/src/com/android/dialer/PhoneCallDetailsHelperTest.java

src/com/android/dialer/calllog/CallLogAdapter.java
@@ -92,6 +92,8 @@ public class CallLogAdapter extends GroupingListAdapter
     private ImageView mBadgeImageView;
     private TextView mBadgeText;
 
+    private String mFilterString;
+
     /** Listener for the primary or secondary actions in the list.
      *  Primary opens the call details.
      *  Secondary calls or plays.
@@ -313,7 +315,7 @@ public class CallLogAdapter extends GroupingListAdapter
         // New items also use the highlighted version of the text.
         final boolean isHighlighted = isNew;
         mCallLogViewsHelper.setPhoneCallDetails(views, details, isHighlighted,
-                mShowSecondaryActionButton);
+                mShowSecondaryActionButton, mFilterString);
 
         int contactType = ContactPhotoManager.TYPE_DEFAULT;
 
@@ -614,5 +616,9 @@ public class CallLogAdapter extends GroupingListAdapter
 
     public String getBetterNumberFromContacts(String number, String countryIso) {
         return mAdapterHelper.getBetterNumberFromContacts(number, countryIso);
+    }
+
+    public void setQueryString(String filter) {
+        mFilterString = filter;
     }
 }